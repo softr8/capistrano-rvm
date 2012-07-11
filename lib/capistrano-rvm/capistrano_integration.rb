04623943e1d345ebac2cf0654df141857b3ae3bd
Michal Papis <mpapis@gmail.com>
2012-07-11 05:14:17 +0300

    cleanup code

--- lib/capistrano-rvm/capistrano_integration.rb
+++ lib/capistrano-rvm/capistrano_integration.rb
@@ -23,25 +23,25 @@ module CapistranoResque
           desc "Installs rvm"
           task :install, :except => {:no_release => true} do
             set :rvm_ruby_string, ''
-            run "RVM=`rvm | grep 'RVM'` ; if [ -z \"$RVM\" ] ; then bash -s stable < <(curl -s https://raw.github.com/wayneeseguin/rvm/master/binscripts/rvm-installer) ; fi", shell: 'bash -l'
+            run "rvm | grep 'RVM' >/dev/null || curl -s https://raw.github.com/wayneeseguin/rvm/master/binscripts/rvm-installer | bash -s stable", shell: 'bash -l'
             set :rvm_ruby_string, tmp_ruby_string
           end
 
           desc "installs ruby version specified in rvm_ruby_string"
           task :install_ruby, :except => {:no_release => true} do
             set :rvm_ruby_string, ''
-            run "RUBY_INSTALLED=`rvm list | grep '#{ruby_version}'` ; if [ -z \"$RUBY_INSTALLED\" ]; then rvm install #{ruby_version} ; fi"
+            run "rvm list strings | grep '^#{ruby_version}' >/dev/null || rvm install #{ruby_version}"
             set :rvm_ruby_string, tmp_ruby_string
           end
 
           desc "sets as default current ruby version"
           task :default_ruby, :except => {:no_release => true} do
-            run "rvm --default #{ruby_version}"
+            run "rvm alias create default #{ruby_version}"
           end
 
           desc "install bundler gem"
           task :install_bundler, :except => {:no_release => true} do
-            run "if [ -z \"`gem list | grep bundler`\" ] ; then gem install bundler --no-rdoc --no-ri ; fi "
+            run "gem list | grep bundler >/dev/null || gem install bundler --no-rdoc --no-ri"
           end
 
           task :create_gemset, :except => {:no_release => true} do
